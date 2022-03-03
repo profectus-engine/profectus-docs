723756ee24aaeb9961e646ae1e166da50173804c
thepaperpilot <thepaperpilot@gmail.com>
2022-03-02 19:35:06 -0600

    Added favicon

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -3,7 +3,11 @@ module.exports = {
   title: 'Profectus',
   description: 'A game engine that grows with you.',
   head: [
-    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono' }]
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }]
   ],
   themeConfig: {
     repo: 'profectus-engine/profectus-docs',
