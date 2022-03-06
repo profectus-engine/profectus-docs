a854c97d15644e45392c5e0cd8309337b0853aa4
thepaperpilot <thepaperpilot@gmail.com>
2022-03-06 04:35:10 +0000

    Deploying to gh-pages from @ profectus-engine/profectus-docs@ce3adae8734e311c61efc05b75500d1d002e94ec 🚀

---
+++ assets/guide_layers.md.53ce88f3.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as t,a as r}from"./app.829490d2.js";const f='{"title":"Layers","description":"","frontmatter":{},"headers":[{"level":2,"title":"Lazy Proxies","slug":"lazy-proxies"}],"relativePath":"guide/layers.md"}',s={},o=r("",6),i=[o];function n(c,l,d,h,y,u){return t(),a("div",null,i)}var _=e(s,[["render",n]]);export{f as __pageData,_ as default};
