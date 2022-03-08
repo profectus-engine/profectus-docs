8b039b3cd7987236e6f432274f4b1c04ad642ea4
thepaperpilot <thepaperpilot@gmail.com>
2022-03-08 01:03:11 -0600

    Generate /api/ sidebar

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 module.exports = {
   lang: "en-US",
   title: 'Profectus',
@@ -62,6 +64,43 @@ module.exports = {
           ]
         }
       ],
+    "/api/": generateAPISidebar()
     }
   }
+}
+
+function generateAPISidebar() {
+  const modules = fs.readdirSync("./api/modules").map(file => file.substr(0, file.length - 3));
+  const folders = {};
+  modules.forEach(file => {
+    // Split by _, but not break_eternity
+    const pieces = file.replace("break_eternity", "break~eternity").split(/_/).map(piece => piece === "break~eternity" ? "break_eternity" : piece);
+    const lineItem = { text: camelToTitle(pieces[pieces.length - 1]), link: `/api/modules/${file}` };
+    pieces.slice(0, pieces.length - 1).reduce((acc, curr) => {
+      // console.log(acc, curr);
+      if (!acc[curr]) {
+        acc[curr] = { text: camelToTitle(curr), children: [] };
+      }
+      return acc[curr].children;
+    }, folders).push(lineItem);
+  });
+  return processFolders(folders);
+}
+
+function camelToTitle(camel) {
+    if (camel === "break_eternity") {
+      return "Break Eternity";
+    }
+    let title = camel.replace(/([A-Z])/g, " $1");
+    title = title.charAt(0).toUpperCase() + title.slice(1);
+    return title;
+}
+
+function processFolders(folders) {
+  return Object.values(folders).map(folder => {
+    if (folder.children) {
+      folder.children = processFolders(folder.children);
+    }
+    return folder;
+  })
 }
