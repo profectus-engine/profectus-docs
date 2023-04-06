f96130b26ba6db5622e061030b703681602d5688
thepaperpilot <thepaperpilot@gmail.com>
2023-04-06 00:15:28 -0500

    Add link to changelog

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -42,7 +42,8 @@ module.exports = {
             { text: "Introduction", link: "/guide/" },
             { text: "Setting Up", link: "/guide/getting-started/setup" },
             { text: "Updating Profectus", link: "/guide/getting-started/updating" },
-            { text: "Example Projects", link: "/guide/getting-started/examples" }
+            { text: "Example Projects", link: "/guide/getting-started/examples" },
+            { text: "Profectus Changelog", link: "https://github.com/profectus-engine/Profectus/blob/main/CHANGELOG.md" }
           ]
         },
         {
